9cafb51b2043a2d4ac39a054ae0be841957d6049
Thomas Chen <chen.thomas.c@gmail.com>
2017-02-17 23:33:54 -0500

    Initial commit

---
+++ lib/timzone.ex
@@ -0,0 +1,20 @@
+defmodule Timzone do
+  use Timex
+
+  @moduledoc """
+  Documentation for Timzone.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Timzone.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/timzone_test.exs
@@ -0,0 +1,8 @@
+defmodule TimzoneTest do
+  use ExUnit.Case
+  doctest Timzone
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
